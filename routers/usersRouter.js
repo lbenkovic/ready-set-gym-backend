f4ea42c0756c04020c1d03035f042dad646a4fe7
Luka Benković <benkovi.luka3@gmail.com>
2024-08-09 00:32:13 +0200

    console log remove

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -3,5 +3,5 @@ import { userProfile } from "../controllers/usersController.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 export const usersRouter = () => {
-    return Router().get("/users/profile", userProfile);
+    return Router().use(authMiddleware).get("/users/profile", userProfile);
 };
