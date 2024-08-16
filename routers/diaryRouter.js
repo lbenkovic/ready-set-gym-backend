d2227f06a3c3b88218ec4cd211e18113228289df
Mirna Pušeljić <puseljicmirna@gmail.com>
2024-08-16 17:05:42 +0200

    fix delete diary

--- routers/diaryRouter.js
+++ routers/diaryRouter.js
@@ -1,15 +1,15 @@
 import { Router } from "express";
 import {
-    recordDiary,
-    getDiary,
-    deleteDiary,
+  recordDiary,
+  getDiary,
+  deleteDiary,
 } from "../controllers/diaryController.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 export const diaryRouter = () => {
-    return Router()
-        .use(authMiddleware)
-        .post("/diary", recordDiary)
-        .get("/diary", getDiary)
-        .delete("/diary", deleteDiary);
+  return Router()
+    .use(authMiddleware)
+    .post("/diary", recordDiary)
+    .get("/diary", getDiary)
+    .delete("/diary/:diaryId", deleteDiary);
 };
