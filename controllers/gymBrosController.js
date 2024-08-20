329f0b9720e76f2dbfdc1cc9e38f1b2b9d4671fe
Luka Benković <benkovi.luka3@gmail.com>
2024-08-20 18:35:51 +0200

    sent requests and logic fixes

--- controllers/gymBrosController.js
+++ controllers/gymBrosController.js
@@ -74,17 +74,41 @@ export const savePendingRequest = async (req, res) => {
     const senderEmail = req.cookies.email;
     const recipientEmail = req.body.email;
 
+    if (senderEmail === recipientEmail) {
+        return res.status(400).json({
+            message: "You cannot send a request to yourself.",
+            data: {},
+        });
+    }
+
     try {
-        const recipientDoc = await gymBrosCollection.findOne(
-            { recipientEmail: recipientEmail },
-            { projection: { acceptedRequests: 1 } }
+        const session = client.startSession();
+        session.startTransaction();
+
+        const senderDoc = await gymBrosCollection.findOne(
+            { recipientEmail: senderEmail },
+            { projection: { pendingRequests: 1, acceptedRequests: 1 } }
         );
 
-        const acceptedRequests = recipientDoc?.acceptedRequests || [];
+        const senderPendingRequests = senderDoc?.pendingRequests || [];
+        const senderAcceptedRequests = senderDoc?.acceptedRequests || [];
 
-        if (acceptedRequests.includes(senderEmail)) {
+        if (senderAcceptedRequests.includes(recipientEmail)) {
+            await session.abortTransaction();
+            session.endSession();
             return res.status(400).json({
-                message: "Gym bro request already accepted.",
+                message:
+                    "You cannot send a request to someone who is already in your accepted requests.",
+                data: {},
+            });
+        }
+
+        if (senderPendingRequests.includes(recipientEmail)) {
+            await session.abortTransaction();
+            session.endSession();
+            return res.status(400).json({
+                message:
+                    "You cannot send a request to someone who has already sent you a request.",
                 data: {},
             });
         }
@@ -94,8 +118,19 @@ export const savePendingRequest = async (req, res) => {
             {
                 $addToSet: { pendingRequests: senderEmail },
             },
-            { upsert: true }
+            { upsert: true, session }
         );
+
+        await gymBrosCollection.updateOne(
+            { recipientEmail: senderEmail },
+            {
+                $addToSet: { sentRequests: recipientEmail },
+            },
+            { upsert: true, session }
+        );
+
+        await session.commitTransaction();
+        session.endSession();
 
         return res.json({
             message: "Pending request saved successfully.",
@@ -113,6 +148,13 @@ export const savePendingRequest = async (req, res) => {
 export const acceptPendingRequest = async (req, res) => {
     const recipientEmail = req.cookies.email;
     const senderEmail = req.body.email;
+
+    if (recipientEmail === senderEmail) {
+        return res.status(400).json({
+            message: "You cannot accept a request from yourself.",
+            data: {},
+        });
+    }
 
     try {
         const session = client.startSession();
@@ -143,6 +185,21 @@ export const acceptPendingRequest = async (req, res) => {
             { session }
         );
 
+        await gymBrosCollection.updateOne(
+            { recipientEmail: senderEmail },
+            {
+                $addToSet: { acceptedRequests: recipientEmail },
+            },
+            { session }
+        );
+        await gymBrosCollection.updateOne(
+            { recipientEmail: senderEmail },
+            {
+                $pull: { sentRequests: recipientEmail },
+            },
+            { session }
+        );
+
         await session.commitTransaction();
         session.endSession();
 
@@ -162,6 +219,13 @@ export const acceptPendingRequest = async (req, res) => {
 export const denyPendingRequest = async (req, res) => {
     const recipientEmail = req.cookies.email;
     const senderEmail = req.body.email;
+
+    if (recipientEmail === senderEmail) {
+        return res.status(400).json({
+            message: "You cannot deny a request from yourself.",
+            data: {},
+        });
+    }
 
     try {
         const session = client.startSession();
@@ -184,6 +248,14 @@ export const denyPendingRequest = async (req, res) => {
             });
         }
 
+        await gymBrosCollection.updateOne(
+            { recipientEmail: senderEmail },
+            {
+                $pull: { sentRequests: recipientEmail },
+            },
+            { session }
+        );
+
         await session.commitTransaction();
         session.endSession();
 
@@ -204,7 +276,17 @@ export const cancelPendingRequest = async (req, res) => {
     const senderEmail = req.cookies.email;
     const recipientEmail = req.body.email;
 
+    if (senderEmail === recipientEmail) {
+        return res.status(400).json({
+            message: "You cannot cancel a request to yourself.",
+            data: {},
+        });
+    }
+
     try {
+        const session = client.startSession();
+        session.startTransaction();
+
         const recipientDoc = await gymBrosCollection.findOne(
             { recipientEmail: recipientEmail },
             { projection: { pendingRequests: 1 } }
@@ -212,19 +294,35 @@ export const cancelPendingRequest = async (req, res) => {
 
         if (
             !recipientDoc ||
+            !recipientDoc.pendingRequests ||
             !recipientDoc.pendingRequests.includes(senderEmail)
         ) {
+            await session.abortTransaction();
+            session.endSession();
             return res.status(404).json({
                 message: "Gym bro request not found.",
                 data: {},
             });
         }
+
         await gymBrosCollection.updateOne(
             { recipientEmail: recipientEmail },
             {
                 $pull: { pendingRequests: senderEmail },
-            }
+            },
+            { session }
         );
+
+        await gymBrosCollection.updateOne(
+            { recipientEmail: senderEmail },
+            {
+                $pull: { sentRequests: recipientEmail },
+            },
+            { session }
+        );
+
+        await session.commitTransaction();
+        session.endSession();
 
         return res.json({
             message: "Gym bro request canceled successfully.",
