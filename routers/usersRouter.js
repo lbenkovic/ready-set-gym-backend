7334c4faa28625eae3e035ca9dc775932c510a42
Mirna Pušeljić <puseljicmirna@gmail.com>
2024-08-23 14:00:53 +0200

    edit route

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -1,15 +1,15 @@
 import { Router } from "express";
 import {
-    getUserProfile,
-    updateUserProfile,
-    searchUsers,
+  getUserProfile,
+  updateUserProfile,
+  searchUsers,
 } from "../controllers/usersController.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 export const usersRouter = () => {
-    return Router()
-        .use(authMiddleware)
-        .get("/user/:email?", getUserProfile)
-        .patch("/user", updateUserProfile)
-        .get("/user/:query", searchUsers);
+  return Router()
+    .use(authMiddleware)
+    .get("/user/:email?", getUserProfile)
+    .patch("/user", updateUserProfile)
+    .get("/users/search/:query", searchUsers);
 };
